41a08a66703c9a4cd5ec5a323ddcd2cf2ef56ecf
Administrator <Administrator@KINGHAZE>
2022-01-30 22:32:22 +0300

    Proje dosyası ekle.

---
+++ ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+
+        }
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        //void ile int'nin farkı, örneğin bu iki örnekte Topla() metoduna int verdiğimiz zaman diğer sayfada çıkan sonucu bir
+        //fonksiyona atayıp tekrardan kullanabiliyoruz.Ancak void ile bu işlemi yapamıyoruz.Sadece bir defa kullanabiliyoruz void ile.
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ProductName'nin özelliklerini böyle de sıralayabiliriz.İkinci yolla da yapabiliriz.İkinci yol daha pratik.
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.UnitInStock = 3;
+            product1.UnitPrice = 500;
+            product1.ProductName = "Masa";
+            product1.CategoryId = 2;
+
+        //Bu şekilde de property'leri yazabiliriz ve bu yukardakinden daha pratik.
+            Product productName2 = new Product { Id = 2, CategoryId = 5, ProductName = "Kalem",
+                UnitPrice = 35, UnitInStock = 5};
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            productManager.Topla2(8,5);//Bunu void ile yazdırdık ve ikinci bir işlem yapamıyoruz.
+
+            int toplamaSonucu = productManager.Topla(6, 9);
+            Console.WriteLine(toplamaSonucu*34);//Bunu int ile yazdırığımız için ikinci bir işlem yapabiliyoruz.Tabi fonksiyona atıyarak.
+
+
+                        
+        }
+    }
+}
